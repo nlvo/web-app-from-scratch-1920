ae9c5402c6b0067d8a196d87347ed80ede4e792e
Lien Vo <lienvo@re-byods-145-109-185-232.wireless.hva.nl>
2020-02-04 13:22:36 +0100

    render data in the overview page

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,14 +1,27 @@
-const title = document.querySelector('ul');
-const thumbnail = document.querySelector('img');
+// const title = document.querySelector('ul');
 
 fetch('https://cors-anywhere.herokuapp.com/http://api.tvmaze.com/schedule?country=KR')
-    .then((response) => {
+    .then(response => {
         return response.json();
     })
-    //anonymouse function with data parameter
-    .then((data) => {
-        console.log(data);
-        for(let i = 0; i < data.length; i++){
-            title.innerHTML += `<li> ${data[i].show.name} </li>`; //template literals
+    //function with data parameter
+    .then(jsonData => {
+        console.log(jsonData);
+        for (let i = 0; i < jsonData.length; i++) {
+            const section = document.querySelector('section');
+            const article = document.createElement('article');
+            const heading = document.createElement('h2');
+            const description = document.createElement('p');
+            const articleImage = document.createElement('img');
+
+            heading.textContent = `${jsonData[i].show.name}`; //template literals
+            description.textContent = `${jsonData[i].name}`;
+            articleImage.src = `${jsonData[i].show.image.medium}`;
+            
+            article.appendChild(articleImage);
+            article.appendChild(heading);
+            article.appendChild(description);
+            section.appendChild(article);
+            // title.innerHTML += `<li> ${jsonData[i].show.name} </li>`; //template literals
         }
     });
