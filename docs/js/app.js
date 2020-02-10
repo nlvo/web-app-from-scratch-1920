1e894e9dfc062f13d617ac9b7138e634127ff22f
Lien Vo <lienvo@re-boyds-145-109-152-223.wireless.hva.nl>
2020-02-10 14:13:20 +0100

    edit fetch data function and add routing with Routie

--- docs/js/app.js
+++ docs/js/app.js
@@ -3,42 +3,55 @@ const apiKey = '&apikey=22b5f2403c91db4fba23cad90a8b2ab7';
 const hash = '&hash=e6bb9dbff35775d2d8aed171d44888d4';
 const timestamp = 'ts=1581025873';
 const comicsUrl = `${endpointMarvel}comics?${timestamp}${apiKey}${hash}`;
-const charactersUrl = `${endpointMarvel}characters?${timestamp}${apiKey}${hash}`;
+// const charactersUrl = `${endpointMarvel}characters?${timestamp}${apiKey}${hash}`;
 
-fetch(comicsUrl)
-    .then(response => {
-        return response.json();
-    }).then(jsonData => {
-        addHtmlElement(jsonData, 'characters');
-    });
-
-fetch(charactersUrl)
-    .then(response => {
-        return response.json();
-    }).then(jsonData => {
-        console.log(jsonData)
-        addHtmlElement(jsonData, 'comics');
-    });
+const getJsonData = (url) => {
+    fetch(url)
+        .then(response => {
+            return response.json();
+        }).then(jsonData => {
+            addHtmlElement(jsonData, 'characters');
+            addHtmlElement(jsonData, 'comics');
+        });
+}
 
     // https://superheroapi.com/api/1776314525838688/search/
 
 const addHtmlElement = (jsonData, selector) => {
     jsonData = jsonData.data.results;
     for (const comic of jsonData) {
-        // console.log(comic);
+        console.log(comic);
         className = `.${selector}`;
         const section = document.querySelector(className);
         const article = document.createElement('article');
         const heading = document.createElement('h2');
         const articleImage = document.createElement('img');
+        const articleUrl = document.createElement('a');
 
         heading.textContent = `${comic.hasOwnProperty('title') ? comic.title : ''}`;
+        articleUrl.textContent = `${comic.hasOwnProperty('title') ? comic.title : ''}`;
         heading.textContent = `${comic.hasOwnProperty('name') ? comic.name : ''}`;
 
         articleImage.src = `${comic.thumbnail.path}.${comic.thumbnail.extension}`; //template literals
+        articleUrl.href = `#characters/${comic.id}`;
 
+        article.appendChild(articleUrl);
         article.appendChild(articleImage);
         article.appendChild(heading);
         section.appendChild(article);
     }
 }
+
+routie({
+    '' : function(){
+        console.log('bleh');
+        getJsonData(comicsUrl);
+    },
+    'characters' : function(){
+
+    },
+    'characters/:id' : function (id){
+        console.log(id);
+
+    }
+})
