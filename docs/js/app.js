9c54d51c501f6870aa860b5ea3332fffb488977c
Lien Vo <lienvo@MacBook-Pro-Lien.local>
2020-02-13 23:24:49 +0100

    add cleanup data function

--- docs/js/app.js
+++ docs/js/app.js
@@ -2,32 +2,46 @@ const endpointMarvel = 'https://gateway.marvel.com/v1/public/';
 const apiKey = '&apikey=22b5f2403c91db4fba23cad90a8b2ab7';
 const hash = '&hash=e6bb9dbff35775d2d8aed171d44888d4';
 const timestamp = 'ts=1581025873';
-const category = 'characters';
-const marvelUrl = `${endpointMarvel}${category}?${timestamp}${apiKey}${hash}`;
-var data;
+const category = 'comics';
+const query = 'dateDescriptor=thisMonth&orderBy=onsaleDate&limit=10'
+const marvelUrl = `${endpointMarvel}${category}?${query}&${timestamp}&${apiKey}&${hash}`;
+// var data;
 
 const getJsonData = (url) => {
     fetch(url)
         .then(response => {
             return response.json();
         }).then(jsonData => {
-            addHtmlElement(jsonData, 'characters');
-            addHtmlElement(jsonData, 'comics');
-            data = jsonData.data.results;
+            const cleanedData = cleanData(jsonData);
+            addHtmlElement(cleanedData, 'characters');
+            addHtmlElement(cleanedData, 'comics');
+            // data = jsonData.data.results;
         });
 }
-
-    // https://superheroapi.com/api/1776314525838688/search/
+// https://superheroapi.com/api/1776314525838688/search/
 
 const getDetail = (id) => {
     const detail = data.filter(data => data.id == id);
     detail = detail[0];
     // https://medium.com/poka-techblog/simplify-your-javascript-use-map-reduce-and-filter-bd02c593cc2d
-    console.log(detail[0]);
+    console.log(detail);
+}
+
+const cleanData = (oldData) => {
+    var newData = oldData.data.results;
+    newData = newData.map((data) => {
+        return {
+            id : data.id,
+            name: data.name || data.title,
+            thumbnail: data.thumbnail
+        }
+    });
+    return newData;
+    // https://stackoverflow.com/questions/54733622/i-need-remove-unnecessary-json-objects-form-my-result-json-file-using-javascript
+
 }
 
 const addHtmlElement = (jsonData, selector) => {
-    jsonData = jsonData.data.results;
 
     for (const comic of jsonData) {
         className = `.${selector}`;
@@ -37,10 +51,8 @@ const addHtmlElement = (jsonData, selector) => {
         const articleImage = document.createElement('img');
         const articleUrl = document.createElement('a');
 
-        heading.textContent = `${comic.hasOwnProperty('title') ? comic.title : ''}`;
-        // articleUrl.textContent = `${comic.hasOwnProperty('title') ? comic.title : ''}`;
+        heading.textContent = `${comic.hasOwnProperty('name') ? comic.name : ''}`;
         articleUrl.textContent = `${comic.hasOwnProperty('name') ? comic.name : ''}`;
-        // heading.textContent = `${comic.hasOwnProperty('name') ? comic.name : ''}`;
 
         articleImage.src = `${comic.thumbnail.path}.${comic.thumbnail.extension}`; //template literals
         articleUrl.href = `#characters/${comic.id}`;
@@ -54,11 +66,7 @@ const addHtmlElement = (jsonData, selector) => {
 
 routie({
     '' : function(){
-        console.log('bleh');
         getJsonData(marvelUrl);
-    },
-    'characters' : function(){
-
     },
     'characters/:id' : function (id){
         getDetail(id);
